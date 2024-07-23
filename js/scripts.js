6efe6440d81195075151d5f3133363b85074edbe
Chace <chace@designsbychace.com>
2024-07-22 21:00:29 -0600

    got RSVP working bare bones

--- js/scripts.js
+++ js/scripts.js
@@ -214,11 +214,12 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
+        console.log (MD5($('#invite_code').val()));
+        if (MD5($('#invite_code').val()) !== 'c67ba7c4c5c0cd4cc3e3a7146fe5c015') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbyo0rEknln8LedEP3bkONsfOh776IR5lFidLhJFQ6jdvRiH4dKvHZmtoIybvnxpxYr2cA/exec', data)
+            //
+            $.post('https://script.google.com/macros/s/AKfycbxugem3qor31e8cVq5BRx1xWmXRAKhDs9dheufBVsmgakbvbjYRAWu1XUqRbyXlZZELAg/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
